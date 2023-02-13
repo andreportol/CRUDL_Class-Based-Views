587689ce6b7b5e38d7bac2e10cd6a7d4296390c6
andreportol <andreportol@gmail.com>
2023-02-13 14:54:11 -0400

    Alteração da porta para 5432, tentando subir a aplicação.

--- areaspublicas_comCBV/settings.py
+++ areaspublicas_comCBV/settings.py
@@ -74,7 +74,7 @@ DATABASES = {
     'USER': 'postgres',
     'PASSWORD':'postgres',
     'HOST': 'localhost',
-    'PORT': '5433',
+    'PORT': '5432',
     }
 }
 
@@ -113,7 +113,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 # Default primary key field type
