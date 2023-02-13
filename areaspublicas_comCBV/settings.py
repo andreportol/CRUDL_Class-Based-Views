b46b2adf7f5c2b22ae40d7b61dc4bb6cdd8e72ec
andreportol <andreportol@gmail.com>
2023-02-13 16:09:01 -0400

    Alterando settings.py para o deploy da aplicação

--- areaspublicas_comCBV/settings.py
+++ areaspublicas_comCBV/settings.py
@@ -2,6 +2,8 @@
 import os
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -10,10 +12,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%zmpx=79=10x^$)-zxk)%jd-t0-d+^41*nvkv!rsr#gp3$4+hx'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -68,14 +70,7 @@ WSGI_APPLICATION = 'areaspublicas_comCBV.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'AreasPublicas',
-    'USER': 'postgres',
-    'PASSWORD':'postgres',
-    'HOST': 'localhost',
-    'PORT': '5433',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
 }
 
 
@@ -107,15 +102,15 @@ TIME_ZONE = 'America/Campo_Grande'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
