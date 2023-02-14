44aa46789fb08753cdc8050d4c1e4c9cd1aae40f
andreportol <andreportol@gmail.com>
2023-02-14 09:31:27 -0400

    Excluindo o bootstrap5

--- areaspublicas_comCBV/settings.py
+++ areaspublicas_comCBV/settings.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
     'django_bootstrap5',
     'django.contrib.humanize', # ajuda na formatação dos valores numericos
     'django_filters',
-    'bootstrap5'
+    
 ]
 
 MIDDLEWARE = [
@@ -109,7 +109,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
