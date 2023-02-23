a3433bb26b1e1bf2aeedde3f886b239f1e043f46
andreportol <andreportol@gmail.com>
2023-02-23 15:49:46 -0400

    Tentando o deploy da aplicação

--- areaspublicas_comCBV/settings.py
+++ areaspublicas_comCBV/settings.py
@@ -12,8 +12,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-#SECRET_KEY = 'django-insecure-%zmpx=79=10x^$)-zxk)%jd-t0-d+^41*nvkv!rsr#gp3$4+hx'
+#SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-%zmpx=79=10x^$)-zxk)%jd-t0-d+^41*nvkv!rsr#gp3$4+hx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
@@ -83,11 +83,11 @@ DATABASES = {
 DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'areaspublicas',
+       'NAME': 'AreasPublicas',
        'USER': 'postgres',
        'PASSWORD': 'postgres',
        'HOST': 'localhost',
-       'PORT': '5432',
+       'PORT': '5433',
    }
 }
 '''
