abeeae9ff74ecd0eee65eeb2ef28694a747d7b2f
andreportol <andreportol@gmail.com>
2023-02-23 18:25:34 -0400

    Deploy railway

--- areaspublicas_comCBV/settings.py
+++ areaspublicas_comCBV/settings.py
@@ -12,8 +12,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY = 'django-insecure-%zmpx=79=10x^$)-zxk)%jd-t0-d+^41*nvkv!rsr#gp3$4+hx'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = 'django-insecure-%zmpx=79=10x^$)-zxk)%jd-t0-d+^41*nvkv!rsr#gp3$4+hx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
